299b5aa7f7519f27e141e439a3b39bedf22e3fb8
takumi1839 <145998662+takumi1839@users.noreply.github.com>
2025-07-11 14:13:17 +0900

    game_managerの作成

---
+++ game_step2.py
@@ -0,0 +1,13 @@
+class Step2:
+    def __init__(self, screen, collected_data):
+        # Step1の結果を引き継ぎ初期化
+
+    def update(self):
+        # じゃがりこ発射、口との当たり判定など
+
+    def draw(self):
+        # 描画処理
+
+    def is_finished(self):
+        # 終了条件（時間やスコア）
+        return True
