374c4991587ae68af1231d99dd42ca29b371f4ec
takumi1839 <145998662+takumi1839@users.noreply.github.com>
2025-07-18 16:26:38 +0900

    cheese完

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import pygame
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 300
 
 def init_screen():
     return pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
